a72e2fa1c9c75b8df0e3754a30726094c3582673
HUAWEI <HUAWEI@LAPTOP-IEH31APK>
2024-07-19 10:11:14 +0300

    Add project files.

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CoreDemo.Controllers
+{
+	public class RegisterController : Controller
+	{
+		WriterManager wm = new WriterManager(new EfWriterRepository());
+
+		[HttpGet]//sayfayüklenice
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]//butonabasılınca
+		public IActionResult Index(Writer p)
+		{
+			WriterValidator wv = new WriterValidator();
+			ValidationResult results=wv.Validate(p);
+			if (results.IsValid)
+			{
+
+				p.WriterStatus = true;
+				p.WriterAbout = "Deneme Test";
+				wm.WriterAdd(p);
+				return RedirectToAction("Index", "Blog");
+			}
+			else
+			{
+				foreach(var item in results.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+				}
+			}
+			return View();
+		}
+	}
+}
---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,31 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents
+{
+	public class CommentList:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var commentValues = new List<UserComment>
+			{
+				new UserComment
+				{
+					ID = 1,
+					UserName="Esra"
+				},
+				new UserComment
+				{
+					ID = 2,
+					UserName="Ayşe"
+				},
+				new UserComment
+				{
+					ID = 3,
+					UserName="Merve"
+				}
+			};
+			return View(commentValues); 
+		}
+	}
+}
